2092ba44ea6e163e2021c0379e288535ceea40ee
david.geirola <david.geirola@moneyfarm.com>
2022-04-02 10:11:45 +0200

    Improve stack-safety

--- core/src/main/scala/cats/xml/Xml.scala
+++ core/src/main/scala/cats/xml/Xml.scala
@@ -3,6 +3,8 @@ package cats.xml
 import cats.MonadThrow
 import org.w3c.dom.{Document as JDocument, Node as JNode, NodeList}
 
+import scala.annotation.tailrec
+
 trait Xml
 object Xml {
 
@@ -12,20 +14,24 @@ object Xml {
   // TODO: JAVA support - to separate
   def fromJavaxDocument(doc: JDocument): XmlNode = {
 
-    // TODO: NO STACK SAFE
+    @tailrec
+    def recScanJNode(left: List[JNode], acc: XmlNode): XmlNode =
+      left match {
+        case Nil                                         => acc
+        case head :: tail if head.getNodeName == "#text" => recScanJNode(tail, acc)
+        case head :: tail => recScanJNode(tail, acc.withChild(rec(head)))
+      }
+
     def rec(ns: JNode): XmlNode = {
       val baseNode: XmlNode = XmlNode(ns.getNodeName)
         .withAttributes(XmlAttribute.fromJavaNodeMap(ns.getAttributes))
 
       if (ns.hasChildNodes) {
-        val childNodes: NodeList   = ns.getChildNodes
-        val len: Int               = childNodes.getLength
-        val result: Array[XmlNode] = new Array[XmlNode](len)
-        for (i <- 0 until len) {
-          result(i) = rec(childNodes.item(i))
-        }
-
-        baseNode.withChildren(result.toList.filterNot(_.label == "#text"))
+        val childNodes: NodeList = ns.getChildNodes
+        recScanJNode(
+          left = Iterator.tabulate(childNodes.getLength)(childNodes.item(_)).toList,
+          acc  = baseNode
+        )
       } else {
         baseNode.withText(ns.getTextContent)
       }
