39b6aa688a450f29acee850600df53a7c7bdc183
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-05-26 00:23:45 +0000

    Update munit-cats-effect-3 to 2.0.0

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -45,8 +45,8 @@ object ProjectDependencies {
 
   object Effect {
     val dedicated: Seq[ModuleID] = Seq(
-      "org.typelevel" %% "cats-effect"         % "3.5.4",
-      "org.typelevel" %% "munit-cats-effect-3" % "1.0.7" % Test
+      "org.typelevel" %% "cats-effect"       % "3.5.4",
+      "org.typelevel" %% "munit-cats-effect" % "2.0.0" % Test
     )
   }
 
